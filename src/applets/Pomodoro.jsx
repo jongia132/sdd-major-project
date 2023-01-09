cab5ab1612f5631ac1290ffc7ec7b5cb22270c77
jongia132 <jongia132@gmail.com>
2023-01-09 14:06:52 +1100

    Convert most code to react

--- src/applets/Pomodoro.jsx
+++ src/applets/Pomodoro.jsx
@@ -1,46 +1,54 @@
 import React from 'react';
+import './Pomodoro.css'
 const Pomodoro = () => {
-    var idiot = null
-    async function timer() {
-        console.log(mins.innerText, secs.innerText)
-        if (secs.innerText < 1 && mins.innerText < 1) {
-            document.getElementById("timer").innerText = "lol"
-            clearInterval(idiot)
-            const sound = new Audio("lol.mp3")
-            sound.play()
-        }
-        if (mins.innerText > 0 && secs.innerText == 0) {
-            mins.innerText -= 1
-            secs.innerText = 59
-        }
-        else {
-            if (secs.innerText <= 10) {
-                secs.innerText = "0" + (secs.innerText -1)
-            }
-            else {
-                secs.innerText -= 1
-            }
-        }
-    }
+    var Time = {
+        mins: 0,
+        secs: 0,
+        count: 0
+    };
+
     function startTimer() {
-        if (document.getElementById('length').value < 1 || document.getElementById('length').value > 99) {
+        setTime(document.getElementById("length").value)
+        if (Time.mins < 1 || Time.mins > 99) {
             return alert("Fuck you")
         }
-        else if (document.getElementById('length').value == 69) {
+        else if (Time.mins == 69) {
             return alert("no")
         }
         document.getElementById("start").setAttribute('disabled', true)
-        mins.innerText = document.getElementById("length").value
-        idiot = setInterval(timer, 1000);
+        let timer = setInterval(function() {
+            if (Time.count < 1) {
+                document.getElementById("timer").innerText = "lol"
+                clearInterval(timer)
+                const sound = new Audio("lol.mp3")
+                sound.play()
+            }
+            else {
+                Time.count -= 1
+                Time.mins = Math.floor(Time.count/60)
+                Time.secs = Time.count % 60
+                console.table(Time)
+            }
+        }, 1000)
+    }
+
+    // Timer components
+    function setTime(mins) {
+        Time.mins = mins
+        Time.count = mins*60
+    }
+
+    function CurrentTime() {
+        return (<span>{Time.mins}:{Time.secs}</span>)
     }
 
     // Return app
     return (
     <div className="pomodoro-timer">
         <h1>Funny Timer</h1>
-        <p className="timer"><span className="mins">0</span>:<span className="secs">00</span></p>
-        <input type="number" min="1" max="99" className="length" step="1"></input>
-        <input type="submit" onClick={startTimer} className="start" value="Start"></input>
+        <p className="timer"><CurrentTime /></p>
+        <input type="number" min="1" max="99" id="length" className="length" step="1"></input>
+        <input type="submit" onClick={startTimer} id="start" className="start" value="Start"></input>
     </div>
     )
 }
