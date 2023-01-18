6e4d95f884e6d942c1cfa5c94253ec736a7eea71
jongia132 <jongia132@gmail.com>
2023-01-18 14:48:58 +1100

    Fixed wait-on

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Index from "./applets/Index"
 import Pomodoro from './applets/Pomodoro';
 
 function App() {
   return (
       <Router>
         <Routes>
+          <Route path='/' element={<Index/>}/>
           <Route path='/pomodoro' element={<Pomodoro/>}/>
         </Routes>
       </Router>
---
+++ src/applets/Index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const Index = () => {
+    return (
+        <a href="/pomodoro">Pomodoro</a>
+    )
+}
+
+export default Index;
