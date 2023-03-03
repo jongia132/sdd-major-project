4105001b764eb8c99b51415751f6e7bbf889b68e
jongia132 <jongia132@gmail.com>
2023-03-03 23:09:38 +1100

    start optimising logic

--- src/applets/Pomodoro.tsx
+++ src/applets/Pomodoro.tsx
@@ -7,52 +7,65 @@ const Pomodoro = () => {
         count: 0
     };
 
-    // function startTimer(mins: number) {
-    //     setTime(mins)
-    //         if (Time.mins < 1 || Time.mins > 99) {
-    //         return alert("Invalid value")
-    //     }
-    //     document.getElementById("start").setAttribute('disabled', true)
-    //     let timer = setInterval(function() {
-    //         if (Time.count < 1) {
-    //             document.getElementById("timer").innerText = "lol"
-    //             clearInterval(timer)
-    //             alert("Done!")
-    //         }
-    //         else {
-    //             Time.count -= 1
-    //             Time.mins = Math.floor(Time.count/60)
-    //             Time.secs = Time.count % 60
-    //             console.table(Time)
-    //             return <span>{Time.mins}:{Time.secs}</span>;
-    //         }
-    //     }, 1000)
-    // }
-
-    // Timer components
-    function setTime(mins: number) {
+    function startTimer(mins: number) {
         Time.mins = mins
+        if (Time.mins < 1 || Time.mins > 99) {
+            return alert("Invalid value")
+        }
+        btnState(!button)
         Time.count = mins*60
+        Timer()
+    }
+
+    function Timer() {
+        let timer = setInterval( () => {
+            if (Time.count > 0) {
+                Time.count -= 1
+                Time.mins = Math.floor(Time.count/60)
+                Time.secs = Time.count % 60
+                console.table(Time)
+                return <span>{Time.mins}:{Time.secs}</span>;
+            }
+            stopTimer()
+        }, 1000)
+    }
+
+    // Start/stop button
+    let [button, btnState] = React.useState(true)
+    function stopTimer() {
+        btnState(!button)
+        clearInterval(timer)
+    }
+    function ToggleButton() {
+        if (button) {
+            return <button id="start" className={styles.button} onClick={stopTimer}>START</button>
+        }
+        else {
+            return <button id="stop" className={styles.button} onClick={stopTimer}>STOP</button>
+        }
     }
 
     // Return app
     return (
-    <div className={styles.component}>
+    <div className={styles.back}>
         <h1>Pomodoro Timer</h1>
         <div className={styles.timer}>
-            <input className={styles.input} type="number" min="0" max="9" step="1"/>
-            <p>:</p>
-            <input className={styles.input} type="number" min="0" max="59" step="5"/>
+            <progress value="0" max="60"/>
+            <section className={styles.digits}>
+                <input className={styles.input} type="number" min="0" max="9" step="1"/>
+                <p>:</p>
+                <Timer />
+                <input className={styles.input} type="number" min="0" max="59" step="5"/>
+            </section>
+            <ToggleButton/>
         </div>
-        <progress value="10" max="60"/>
-        <button type="submit" id="start" className={styles.button} value="Start"></button>
     </div>
     )
 }
 
 function PomodoroWidget() {
     return (
-        <p>THIS IS WORKING!</p>
+        <p>Widget loaded</p>
     );
 }
 export {Pomodoro, PomodoroWidget}
