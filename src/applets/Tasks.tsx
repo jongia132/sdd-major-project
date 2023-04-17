0a3eff1347e452751df9e70f5fc2500d7cf1473f
Jonathan Giang <jongia132@gmail.com>
2023-04-17 12:44:26 +1000

    Add more database functions

--- src/applets/Tasks.tsx
+++ src/applets/Tasks.tsx
@@ -4,51 +4,65 @@ import { openDB } from 'idb'
 import React from "react"
 const Tasks = () => {
     // Allow global database transaction access
-    let db, currentList, currentTab:string
+    let db: IDBDatabase, currentList, currentTab: string
 
     // Avoid type errors
     interface dbTypes {
         group: string
     }
+
     // Create or verify existance of database
-    let dataStore = indexedDB.open("Tasks")
     let [group, setGroup] = React.useState("default")
+    let dataStore = indexedDB.open("Tasks")
+    dataStore.onerror = e => {
+        console.error("Database error")
+        return (<p>ERROR</p>)
+    }
+    dataStore.onupgradeneeded = function () {
+        db = dataStore.result
+        if (!db.objectStoreNames.contains("default")) {
+            let store:IDBObjectStore = db.createObjectStore("default", { keyPath: "id" })
+            store.createIndex("name", ["description", "example"])
+        }
+        console.warn("Database upgraded")
+    }
     function ReadTable() {
-        dataStore.onupgradeneeded = function() {
-            console.log("upgraded")
-        }
-        dataStore.onsuccess = function() {
-            console.log("Database found")
+        dataStore.onsuccess = function () {
+            console.info("Database found")
             db = dataStore.result
-            if (!db.objectStoreNames.contains("default")) {
-                db.createObjectStore("default")
-            }
-            currentList = db.transaction([group], "readonly").objectStore("default")
+            currentList = db.transaction("default", "readonly")
+            return (
+                <p>Success!</p>
+            )
         }
-        return(
-            null
-        )
     }
 
-    function updateTask() {}
+    function updateTask() { }
 
-    function addTask() {}
+    function addTask() { }
 
-    function deleteTask() {}
+    function deleteTask() { }
 
-    function test() {console.log({currentTab})}
+    function test() { console.log({ currentTab }) }
+
+    function resetDatabase() {
+        let deletion = window.indexedDB.deleteDatabase("Tasks")
+        deletion.onsuccess = function() {console.warn("Database deleted")}
+        deletion.onerror = function() {console.error("Error deleting database")}
+    }
 
     return (
-        <div className="content">
+        <div className={`${"content"} ${styles.container}`}>
             <h1>Your tasks</h1>
             <Toolbar size="small">
                 <ToolbarButton appearance="primary">Add task</ToolbarButton>
+                <ToolbarButton appearance="subtle" onClick={resetDatabase}>DEBUG ONLY: Reset database</ToolbarButton>
             </Toolbar>
             <TabList onTabSelect={test} defaultSelectedValue={"default"} size="large">
                 <Tab value="default">Tasks</Tab>
                 <Tab value="lol">TT</Tab>
             </TabList>
-            <ReadTable/>
+            <ReadTable />
         </div>
     )
 }
